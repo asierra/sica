287d1ad3c7f3d5c88cf09f3247a6d230a50d559a
Alejandro Sierra <algsierra@gmail.com>
2018-06-15 14:41:02 -0500

    dded column account to entries report.

--- app/models/student_entry.rb
+++ app/models/student_entry.rb
@@ -2,14 +2,13 @@ class StudentEntry < ApplicationRecord
   belongs_to :student
 
   def self.to_csv
-    attributes = %w{Estudiante Entrada}
+    attributes = %w{Estudiante Cuenta Entrada}
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
       
       all.each do |student|
-        csv <<  [ student.student.full_name, student.entry.to_s ]
-#        csv << attributes.map{ |attr| user.send(attr) }
+        csv <<  [ student.student.full_name, student.student.account, student.entry.to_s ]
       end
     end
   end
